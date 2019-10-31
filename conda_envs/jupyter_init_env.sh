000d8ed197a95e45f0630f8daa2164e892ca6434
Ethan Ho <eho@tacc.utexas.edu>
2019-10-31 14:32:55 -0500

    Clean up conda envs from development

--- conda_envs/jupyter_init_env.sh
+++ conda_envs/jupyter_init_env.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-new_env="test1"
-new_env_yml=$1
+new_env=$1
+new_env_yml=$2
 
+if [ ! -f ${new_env_yml} ]; then
+    echo "No conda env YAML file exists at ${new_env_yml}"
+    exit 1
+fi
+if [ -d $LOCAL_ENVS/${new_env} ]; then
+    echo "Directory at $LOCAL_ENVS/${new_env} already exists"
+    exit 1
+fi
 
 conda env list
 echo "Creating conda env ${new_env} from file ${new_env_yml}"
