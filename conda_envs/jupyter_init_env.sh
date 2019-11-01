143065c51594043c4be10b7aacdbf9936bf2cead
Ethan Ho <eho@tacc.utexas.edu>
2019-11-01 08:48:02 -0500

    Improved logging, error handling, defaults handling

--- conda_envs/jupyter_init_env.sh
+++ conda_envs/jupyter_init_env.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 new_env=$1
 new_env_yml=$2
 
+# Handle defaults
+if [ -z ${new_env_yml} ]; then
+    echo "No YAML file passed. Reverting to default..."
+    new_env_yml=${HERE}/template_notebook_env.yml
+fi
+filename=$(basename -- ${new_env_yml})
+if [ -z ${new_env} ]; then
+    echo "No env name passed. Reverting to default..."
+    new_env="${filename%.*}"
+fi
+
+
+# Sanity checks
 if [ ! -f ${new_env_yml} ]; then
     echo "No conda env YAML file exists at ${new_env_yml}"
     exit 1
@@ -12,9 +26,17 @@ if [ -d $LOCAL_ENVS/${new_env} ]; then
     exit 1
 fi
 
+# Create conda env from file
 conda env list
 echo "Creating conda env ${new_env} from file ${new_env_yml}"
 conda env create --prefix $LOCAL_ENVS/${new_env} --file ${new_env_yml}
+create_exited=$?
+if [ ${create_exited} -ne 0 ]; then
+    echo "ERROR: conda env creation exited with code ${create_exited}"
+    exit ${create_exited}
+fi
+
+# Install kernel for Jupyter
 conda env list
 echo "Creating kernel file for conda env ${new_env}..."
 $LOCAL_ENVS/${new_env}/bin/python -m ipykernel install \
